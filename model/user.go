8e1d77450f98517ef6bb6006529d43842b9e362b
Hazim Arafa <hazimarafa80@gmail.com>
2021-06-06 19:37:57 -0700

    Update user.go

--- model/user.go
+++ model/user.go
@@ -5,14 +5,14 @@ import (
 )
 
 type User struct {
+	Sessions  []Session  `gorm:"foreignKey:UserRefer; constraint:OnUpdate:CASCADE, OnDelete:CASCADE;" json:"-"`
+	Products  []Product  `gorm:"foreignKey:UserRefer; constraint:OnUpdate:CASCADE, OnDelete:CASCADE;" json:"-"`
 	ID		  guuid.UUID `gorm:"primaryKey" json:"-"`
 	Token	  guuid.UUID `json:"-"`
 	Image	  string 	 `json:"image"`
 	Username  string     `json:"username"`
 	Email     string     `json:"email"`
 	Password  string     `json:"-"`
-	Sessions  []Session  `gorm:"foreignKey:UserRefer; constraint:OnUpdate:CASCADE, OnDelete:CASCADE;" json:"-"`
-	Products  []Product  `gorm:"foreignKey:UserRefer; constraint:OnUpdate:CASCADE, OnDelete:CASCADE;" json:"-"`
 	CreatedAt int64      `gorm:"autoCreateTime" json:"-" `
 	UpdatedAt int64      `gorm:"autoUpdateTime:milli" json:"-"`
 }
